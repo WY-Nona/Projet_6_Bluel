e54b7494ac02b8ffd2c110d2cc5015084c344158
WY-Nona <nona.belioute@gmail.com>
2024-02-21 21:25:38 +0100

    Login_Page_HTML/CSS

--- FrontEnd/assets/javascript/script.js
+++ FrontEnd/assets/javascript/script.js
@@ -29,6 +29,19 @@ function displayWorkCards(works) {
     figure.appendChild(figcaption);   // Génération de la balise figcaption à la figure
   }) 
 }
+
+// Appel les données des travaux
+export async function fetchWorksData() {
+  try {
+      const url = 'http://localhost:5678/api/works';
+      const response = await fetch(url);
+
+      return await response.json();
+  } catch (error) {
+      console.log('===> error', error);
+  }
+}
+
 //Appel les catégories liés aux travaux
 export async function fetchCategoriesData() {
   try {
@@ -52,19 +65,6 @@ export async function fetchCategoriesData() {
       parent.appendChild(div);
     });
   }
-// Appel les données des travaux
-export async function fetchWorksData() {
-  try {
-      const url = 'http://localhost:5678/api/works';
-      const response = await fetch(url);
-
-      return await response.json();
-  } catch (error) {
-      console.log('===> error', error);
-  }
-}
-
-
 
 
 // Gére principalement les filtres, l'affichage des travaux et modifie le style lors du clic
